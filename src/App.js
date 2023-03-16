bf61c3ee3eb58b74e449c5638f6f3e5d83552a8b
Shanthanu <shandroid@DESKTOP-0RDJO38.localdomain>
2023-03-17 00:01:46 +0530

    wallet connection

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { ethers } from 'ethers';
 
 function App() {
+  async function connect(){
+    if(typeof window.ethereum!=undefined){
+      window.ethereum.request({method:"eth_requestAccounts"}).then((e)=>console.log('connected'))
+    }
+    else{
+      alert("please install metamask to connect your wallet")
+    }
+  }
+
+  function fund(amount){
+    console.log(`funding ${amount}...`);
+    const provider = new ethers.providers.JsonRpcProvider(window.ethereum);
+    const signer = provider.getSigner();
+    console.log(signer)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <button className='btn btn-sm btn-success' onClick={connect}>Connect</button>
+    <button className='btn btn-sm btn-success' onClick={fund}>Fund</button>
     </div>
   );
 }
