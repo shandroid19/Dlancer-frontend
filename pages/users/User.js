961349af9eb262f349201db34396092db25f3291
Shanthanu <shandroid@DESKTOP-0RDJO38.localdomain>
2023-04-19 22:28:33 +0530

    truncated skills in usercard

--- pages/users/User.js
+++ pages/users/User.js
@@ -1,9 +1,56 @@
 import Link from "next/link";
 import styles from '../../styles/Users.module.css'
+import { useState } from "react";
+import { useRouter } from "next/router";
+const skillsets = [
+    'React.js',
+    'AngularJS',
+    'Vue.js',
+    'Ember.js',
+    'Backbone.js',
+    'Node.js',
+    'Django',
+    'Spring Boot',
+    'Flutter',
+    'React Native',
+    'Xamarin',
+    'Swift',
+    'Kotlin',
+    'MySQL',
+    'MongoDB',
+    'Oracle',
+    'PostgreSQL',
+    'SQL Server',
+    'Amazon Web Services (AWS)',
+    'Microsoft Azure',
+    'Google Cloud Platform (GCP)',
+    'Docker',
+    'Kubernetes',
+    'Jenkins',
+    'Ansible',
+    'Terraform',
+    'JavaScript',
+    'Python',
+    'Java',
+    'Ruby',
+    'C++',
+    'Jest',
+    'Selenium',
+    'Cypress',
+    'JUnit',
+    'Pytest',
+    'TensorFlow',
+    'PyTorch',
+    'Scikit-learn',
+    'Keras',
+    'OpenCV'
+    ];
 export default function User({id,username,tasks,skills,image}){
+    const [showMoreSkills,setShowMoreSkills] = useState(false)
+    const router = useRouter()
     return<>
-        <Link className={styles.cardlink} href={`/users/${id}`}>
-        <div className="card shadow">
+        {/* <Link className={styles.cardlink} href={`/users/${id}`}> */}
+        <div style={{height:'100%'}} className="card shadow">
             <div className="card-body">
                 <div className="container">
                     <div className="row d-flex justify-content-center ">
@@ -27,11 +74,42 @@ export default function User({id,username,tasks,skills,image}){
                                 <div className="col-sm-6">
                                     Skills:
                                 </div>
-                                <div className="col-sm-6">
+                                {/* <div className="col-sm-6">
                                     {skills.map((skill,key)=>{
-                                        return <span key={key} className="badge bg-secondary">{skill}</span>
+                                        return <span key={key} className="badge bg-secondary">{skillsets[skill]}</span>
                                     })}
-                                </div>
+                                </div> */}
+
+
+                <div className="col-sm-6" style={{overflowY:'scroll',maxHeight:'6rem'}}>
+                {skills.slice(0, 3).map((skill, key) => {
+                return (
+                <span key={key} className="badge bg-secondary">
+             {skillsets[skill]}
+                 </span>
+                    );
+                })}
+            {showMoreSkills &&
+                skills.slice(3).map((skill, key) => {
+                  return (
+                     <span key={key} className="badge bg-secondary">
+                         {skillsets[skill]}
+                        </span>
+                    );
+                    })}
+                {skills.length > 3 && (
+                <a href="#" className="d-block" onClick={() => setShowMoreSkills(!showMoreSkills)}>
+                    {showMoreSkills ? "See less" : "See more"}
+            </a>
+                )}
+                </div>
+                <div className="row">
+                    <div className="col-md-2 col-sm-4">
+                        <button onClick={()=>router.push(`/users/${id}`)} className="btn btn-primary btn-small text-white">
+                            View 
+                        </button>
+                    </div>
+                </div>
                             </div>
                         </div>
 
@@ -40,6 +118,6 @@ export default function User({id,username,tasks,skills,image}){
                 </div>
             </div>
         </div>
-        </Link>
+        {/* </Link> */}
     </>
 }
--- pages/users/Users.js
+++ pages/users/Users.js
@@ -1,21 +1,78 @@
 import User from "./User";
-import { useState } from "react";
-
+import { useState,useEffect } from "react";
+const skillsets = [
+  'React.js',
+  'AngularJS',
+  'Vue.js',
+  'Ember.js',
+  'Backbone.js',
+  'Node.js',
+  'Django',
+  'Spring Boot',
+  'Flutter',
+  'React Native',
+  'Xamarin',
+  'Swift',
+  'Kotlin',
+  'MySQL',
+  'MongoDB',
+  'Oracle',
+  'PostgreSQL',
+  'SQL Server',
+  'Amazon Web Services (AWS)',
+  'Microsoft Azure',
+  'Google Cloud Platform (GCP)',
+  'Docker',
+  'Kubernetes',
+  'Jenkins',
+  'Ansible',
+  'Terraform',
+  'JavaScript',
+  'Python',
+  'Java',
+  'Ruby',
+  'C++',
+  'Jest',
+  'Selenium',
+  'Cypress',
+  'JUnit',
+  'Pytest',
+  'TensorFlow',
+  'PyTorch',
+  'Scikit-learn',
+  'Keras',
+  'OpenCV'
+  ];
 export default function Users(){
     const image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpCKq1XnPYYDaUIlwlsvmLPZ-9-rdK28RToA&usqp=CAU";
 
   
     const data = [
-        {username:"employee",id:"0x1CBd3b2770909D4e10f157cABC84C7264073C9Ec",bio:"hardworker",skills:["React.js","Python","Tensorflow"],tasks:"5/9",image:image},
-        {username:"shandroid",id:"0xFABB0ac9d68B0B445fB7357272Ff202C5651694a",bio:"hardworker",skills:["ethers","flutter","linux"],tasks:"5/9",image:image},
-        {username:"owner",id:"0xdF3e18d64BC6A983f673Ab319CCaE4f1a57C7097",bio:"hardworker",skills:["SQL","django","angular"],tasks:"5/9",image:image}
+        {username:"employee",id:"0x1CBd3b2770909D4e10f157cABC84C7264073C9Ec",bio:"hardworker",skills:[0,1,2,3],tasks:"5/9",image:image},
+        {username:"shandroid",id:"0xFABB0ac9d68B0B445fB7357272Ff202C5651694a",bio:"hardworker",skills:[1,2,3,4,5],tasks:"5/9",image:image},
+        {username:"owner",id:"0xdF3e18d64BC6A983f673Ab319CCaE4f1a57C7097",bio:"hardworker",skills:[10,12,15,16],tasks:"5/9",image:image}
     ];
+
+
+    useEffect(()=>{
+      fetch('http://localhost:5000/api/users/').then((res)=>{
+        return res.json(); 
+      }).then((res)=>{
+        console.log(res)
+        // const result = res.skills.map((index) => skillsets[index]);
+        // res.skills= result
+        console.log(res)
+        // setData(res)
+      }).catch((e)=>console.log(e))
+    },[]) 
+
+
     const [filterText, setFilterText] = useState("");
 
     function UsersList({data, filterText}) {
         const filteredData = data.filter((project) => {
           const titleMatch = project.username.toLowerCase().includes(filterText.toLowerCase());
-          const skillsMatch = project.skills.some((skill) => skill.toLowerCase().includes(filterText.toLowerCase()));
+          const skillsMatch = project.skills.some((skill) => skillsets[skill].toLowerCase().includes(filterText.toLowerCase()));
           const idMatch = project.id.toLowerCase().includes(filterText.toLowerCase());
           return titleMatch || skillsMatch || idMatch;
         });
